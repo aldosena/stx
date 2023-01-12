285ed1d4465ca64618dd813e4257e46cc018f736
aldosena <36321317+aldosena@users.noreply.github.com>
2023-01-12 13:43:42 -0300

    Update stx.php

--- stx.php
+++ stx.php
@@ -2,9 +2,10 @@
 # Esta função reune várias formas de tirar caracteres especias da string;
 # Pode converter a string para maiúscula ou minúscula
 # Contato: aldosena10@gmail.com
-# Alterado em: 12/08/2021
+# Alterado em: 12/01/2022
+
 # Fonte: https://github.com/aldosena/stx/edit/main/stx.php
-function stx($texto, $letra){
+function stx($texto, $letra="x"){
 	$e = trim($texto);
 	$ero = str_replace("'","",$e);	
 	$erotic = str_replace('"','',$ero);  
